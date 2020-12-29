17df3d78fc5d2864d69484b92ab7992abb0dc6ec
danilovxp <danilovxp@gmail.com>
2020-12-28 17:54:21 -0800

    Updates

---
+++ precache-manifest.04eb274790d83d2bb97a7260cfad4056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbc742f3e6973fc74c1fe1e35c5557a",
+    "url": "/v3/index.html"
+  },
+  {
+    "revision": "1d839ef4ddfd56741c27",
+    "url": "/v3/static/css/main.e6ab051c.chunk.css"
+  },
+  {
+    "revision": "fe6da3fd7b1129410ed8",
+    "url": "/v3/static/js/2.a46923cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/v3/static/js/2.a46923cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d839ef4ddfd56741c27",
+    "url": "/v3/static/js/main.5af50395.chunk.js"
+  },
+  {
+    "revision": "13a9794c4a58cf3f90a9",
+    "url": "/v3/static/js/runtime-main.8d3e8f3a.js"
+  }
+]);
